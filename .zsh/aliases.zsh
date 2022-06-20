8feb4d47783fb8aae246b4ef48a80863fe1f3298
Kenji <leokk06@gmail.com>
2022-06-20 13:29:28 -0300

    ✨ Add python aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,3 +6,9 @@ alias srestart="sudo systemctl restart"
 alias sdisable="sudo systemctl disable"
 alias senable="sudo systemctl enable"
 alias sstatus="systemctl status"
+# Python
+mkpyvenv() {
+    python${1:-3} -m venv .venv
+    source .venv/bin/activate
+}
+alias pipreq="python3 -m pip install -r requirements.txt -U pip"
